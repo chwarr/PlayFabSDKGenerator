fd4ba2671f0d36b4f92486ba3c5bbb286ab900b9
John LaFontaine <johlafo@microsoft.com>
2021-11-22 12:08:03 -0800

    Adding build script for PlayFab C SDK

---
+++ SDKBuildScripts/Windows/c_build.bat
@@ -0,0 +1,41 @@
+set SdkName=PlayFabCoreCSdk
+
+cd ..
+
+set destPath=..\sdks\%SdkName%
+
+pushd ..\%destPath%
+if [%delSrc%] == [true] (
+    del /S *.as
+    del /S *.cpp
+    del /S *.cs
+    del /S *.h
+    del /S *.java
+    del /S *.js
+    del /S *.lua
+    del /S *.m
+    del /S *.php
+    del /S *.py
+    del /S *.ts
+    attrib -H *.meta /S /D
+)
+popd
+
+if ["%apiSpecSource%"] == [""] (
+    set apiSpecSource=-apiSpecGitUrl
+)
+
+if defined NODE_NAME (
+    set buildIdentifier=JBuild_%SdkName%_%NODE_NAME%_%EXECUTOR_NUMBER%
+)
+
+cd %~dp0
+pushd ..
+pushd ..
+rem === BUILDING %SdkName% with params %* ===
+node generate.js -destPath %destPath% %apiSpecSource% -buildIdentifier %buildIdentifier% %*
+popd
+popd
+
+pause
+
